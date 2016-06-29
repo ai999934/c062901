256b4f676dc3a535b36c144c5537aa3acfab5597
金井則通(Norimichi Kanai) <k.norimichi9999@i.softbank.jp>
2016-06-29 16:56:11 +0900

    Create c062904.cpp

---
+++ c062904.cpp
@@ -0,0 +1,43 @@
+// -> http://www11.atwiki.jp/kenichiro/pages/32.html
+#include <stdio.h>
+using namespace std;
+
+void euler(double&, double&, double, double);
+
+main()
+{
+    double x, v, f, h, t, tend;
+    int step;
+    
+    x = 1.0;
+    v = 0.0;
+    f = 0;
+    tend = 100;
+    h = 0.1;
+    
+    step = (int)(tend / h);
+    
+    for(int i = 0; i < step; i++)
+    {
+        euler(x, v, f, h);
+        printf("t=%f,", t);
+        printf("x=%f", x);
+        printf("\n");
+        t += h;
+    }
+}
+
+void euler(double &x, double &v, double f, double h)
+{
+    double dx, dv;
+    
+    dx = v;
+    dv = -f * v * (x * x - 1) - x;
+    
+    x = x + h * dx;
+    v = v + h * dv;
+}
+
+// g++ c062904.cpp libglibw32-3.a -IC:\mingw-jp-20040217\include\ -LC:\mingw-jp-20040217\lib\ -IC:\Glibw32\ -LC:\Glibw32\ -luser
+// rename a.exe c062904.*
+// c062904.exe > c062904.txt
